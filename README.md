e96586e92de87c20ec898927cbb08e31e75db76a
Joao Victor <jv.duarte.viana@gmail.com>
2022-06-12 13:42:18 -0300

    correção da api e README criado

---
+++ README.md
@@ -0,0 +1,29 @@
+# Projeto de Obtenção dos dados do CNES
+
+## Requerimentos:
+
+- Python 3.8 ou mais
+- Python Pip
+- Python Flask
+
+## Instalar
+
+pip -r ./requirements.tsx
+
+## Como utilizar
+
+Antes de rodar a aplicação é necessário setar o valor da constante FLASK_APP:
+
+export set FLASK_APP=app
+
+Para rodar a aplicação basta rodar o comando:
+
+flask run
+
+Ao rodar a aplicação, será liberado uma url para fazer o download dos arquivos da CNES em csv, para isso, entre no seu navegador e acesse:
+
+http://localhost:5000/download/PF/CE/2021/07
+
+O padrão dos paramêtros são
+
+http://localhost:5000/download/tipo/uf/ano/mes
--- app.py
+++ app.py
@@ -1,49 +1,15 @@
 from cnes import download
-from flask import Flask, json
+from flask import Flask, make_response
 import numpy as np
 
 app = Flask(__name__)
 
-class NpEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, np.integer):
-            return int(obj)
-        if isinstance(obj, np.floating):
-            return float(obj)
-        if isinstance(obj, np.ndarray):
-            return obj.tolist()
-        return super(NpEncoder, self).default(obj)
-
-@app.route("/<tipo>/<uf>/<ano>/<mes>/<page>/<per_page>")
-def hello_world(tipo: str, uf: str, ano: int, mes: int, page: int, per_page: int):
-    per_page = int(per_page)
-    page = int(page)
-    data_dict = dict()
-
+@app.route("/download/<tipo>/<uf>/<ano>/<mes>")
+def cnes_download(tipo: str, uf: str, ano: int, mes: int):
     df = download(tipo, uf, ano, mes)
-
-    index_start = ((page - 1) * per_page)
-    index_final = index_start + per_page
-
-    data_dict['quantidade'] = len(df)
-    data_dict['paginas'] = len(df) // per_page
-    data_dict['resultados'] = []
-
-    df = df.iloc[index_start:index_final,:]
-
-    for row in range(per_page):
-        row_dict = dict()
-        
-        for col_index in range(len(df.columns)):
-            row_dict[df.columns[col_index]] = df.iloc[row,col_index]
-        
-        data_dict['resultados'].append(row_dict)
-
-    response = app.response_class(
-        response=json.dumps(data_dict, cls=NpEncoder, indent=4),
-        status=200,
-        mimetype='application/json'
-    )
+    response = make_response(df.to_csv())
+    response.headers["Content-Disposition"] = "attachment; filename={}{}{}{}.csv".format(tipo, uf, ano, mes)
+    response.headers["Content-type"] = "text/csv"
 
     return response
 
